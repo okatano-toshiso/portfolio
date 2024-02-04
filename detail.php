88209bc427e1c8225b6711b1f0c794056b21a2a6
okatano-toshiso <web@nuevo.jp>
2024-02-04 12:47:38 +0900

    [refactering] detail.php

--- detail.php
+++ detail.php
@@ -17,147 +17,44 @@
 		</div>
 		<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 text-left">
 			<div class="haba90">
-				<p><?php the_field('information'); ?></p>
-				<p>制作種別:<?php echo the_field('brand'); ?></p>
-				<p>url:<?php echo the_field('url'); ?></p>
-				<p>役割:<?php echo the_field('role'); ?></p>
-				<p>依頼主:<?php echo the_field('client'); ?></p>
-				<p>言語:<?php echo the_field('language'); ?></p>
-				<p>CMS:<?php echo the_field('cms'); ?></p>
-				<p>フレームワーク：<?php echo the_field('framework'); ?></p>
-				<p>スマホ対応：<?php echo the_field('smartphone'); ?></p>
-				<p>サーバー：<?php echo the_field('server'); ?></p>
+			<?php
+				$field_mappings = array(
+					'information' => '', 'brand' => '制作種別：', 'url' => 'url：', 'role' => '役割：', 'client' => '依頼主：', 'language' => '言語：', 'cms' => 'CMS：', 'framework' => 'フレームワーク：', 'smartphone' => 'スマホ対応：', 'server' => 'サーバー：'
+				);
+				foreach ($field_mappings as $field => $label) {
+					$value = get_field($field);
+					if (!empty($value)) {
+						echo '<p>' . $label .(is_array($value) ? implode(', ', $value) : $value) . '</p>';
+					}
+				}
+			?>
 			</div>
 		</div>
 	</div>
 	<div class="pt_90">
 		<?php echo the_field('program'); ?>
 	</div>
-	<?php if(!empty(get_post_meta($post->ID, 'image01', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image01'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image01'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image02', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image02'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image02'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image03', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image03'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image03'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image04', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image04'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image04'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image05', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image05'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image05'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image06', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image06'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image06'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image07', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image07'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image07'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image08', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image08'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image08'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image09', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image09'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image09'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image10', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image10'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image10'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image11', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image11'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image11'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image12', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image12'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image12'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image13', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image13'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image13'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image14', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image14'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image14'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image15', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image15'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image15'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image16', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image16'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image16'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image17', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image17'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image17'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
-	<?php if(!empty(get_post_meta($post->ID, 'image18', true))):?>
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
-			<div class="frame02">
-				<a href="<?php the_field('image18'); ?>" target="_blank"><div class="garallyimg02" style="background-image: url(<?php the_field('image13'); ?>);"></div></a>
-			</div>
-		</div>
-	<?php endif;?>
+	<?php
+		$imageFields = array(
+			'image01', 'image02', 'image03', 'image04', 'image05',
+			'image06', 'image07', 'image08', 'image09', 'image10',
+			'image11', 'image12', 'image13', 'image14', 'image15',
+			'image16', 'image17', 'image18'
+		);
+		foreach ($imageFields as $imageField) :
+			$imageValue = get_post_meta($post->ID, $imageField, true);
+			if (!empty($imageValue)) :
+				?>
+				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 text-center">
+					<div class="frame02">
+						<a href="<?php the_field($imageField); ?>" target="_blank">
+							<div class="garallyimg02" style="background-image: url(<?php the_field($imageField); ?>);"></div>
+						</a>
+					</div>
+				</div>
+				<?php
+			endif;
+		endforeach;
+	?>
 </div>
 <?php get_footer(); ?>
